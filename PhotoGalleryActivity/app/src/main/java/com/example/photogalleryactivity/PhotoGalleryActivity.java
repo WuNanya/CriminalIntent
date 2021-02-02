b2762812baa2e91c7a4c30c1dad68b8ee96fa5b2
吴楠 <wunan@wunandeMBP.lan>
2021-02-02 22:51:56 +0800

    http请求及后台任务

---
+++ PhotoGalleryActivity/app/src/main/java/com/example/photogalleryactivity/PhotoGalleryActivity.java
@@ -0,0 +1,13 @@
+package com.example.photogalleryactivity;
+
+import androidx.fragment.app.Fragment;
+
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFrgment.newInstnce();
+    }
+}
---
+++ PhotoGalleryActivity/app/src/main/java/com/example/photogalleryactivity/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.example.photogalleryactivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (null == fragment) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
